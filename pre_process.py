838a80d185032e47cee7959a5f16a87bb6d9b8b9
caiyueliang <393900414@qq.com>
2019-04-18 09:48:31 +0800

    update

--- pre_process.py
+++ pre_process.py
@@ -87,7 +87,7 @@ def update_image_path(root_path, label_file):
 
 
 # 每个类别里提取n张图片放到一个文件夹里
-def data_pre_process_2(root_path, output_path, count):
+def data_pre_process_2(root_path, output_path, start_count, end_count):
     mkdir_if_not_exist(output_path)
 
     mkdir_if_not_exist(output_path)
@@ -108,13 +108,14 @@ def data_pre_process_2(root_path, output_path, count):
                 output_path_1 = os.path.join(output_path, dir)
                 mkdir_if_not_exist(output_path_1)
 
-                if i < count:
+                if start_count <= i < end_count:
                     shutil.copy(file_path, os.path.join(output_path_1, file))
-                    i += 1
                     label_file = file_path.replace(file_path.split('.')[-1], 'txt')
                     if os.path.exists(label_file):
                         print(label_file)
                         shutil.copy(label_file, output_path_1)
+
+                i += 1
     return
 
 
@@ -141,7 +142,8 @@ def parse_argvs():
     parser.add_argument("--func", dest="func", type=int, default=0)
     parser.add_argument("--root_path", dest="root_path", type=str, default="../Data/AI比赛/特定物品识别/images/")
     parser.add_argument("--output_path", dest="output_path", type=str, default="../Data/AI比赛/特定物品识别/images_100/")
-    parser.add_argument("--count", dest="count", type=int, default=100)
+    parser.add_argument("--start_count", dest="start_count", type=int, default=0)
+    parser.add_argument("--end_count", dest="end_count", type=int, default=100)
     args = parser.parse_args()
     print(args)
     # logger.warning(args)
@@ -161,7 +163,7 @@ if __name__ == '__main__':
         update_image_path(os.path.join(args.output_path, 'test'), 'image_path.txt')
     elif args.func == 2:
         # 每个类别里提取n张图片放到一个文件夹里
-        data_pre_process_2(args.root_path, args.output_path, args.count)
+        data_pre_process_2(args.root_path, args.output_path, args.start_count, args.end_count)
     elif args.func == 3:
         # 生成image_path.txt
         update_image_path(args.root_path, 'image_path.txt')
